8bdb52c3b93002c97b4040fcebb21ee118afcac4
Bradley Andrews <bandrewsny@gmail.com>
2022-01-25 12:10:29 -0700

    version 8

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited and saved 7.
+          Edited and saved 8.
         </p>
         <a
           className="App-link"

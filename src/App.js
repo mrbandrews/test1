b0d3297534df246b3f7297c386cefb683aef7794
Bradley Andrews <bandrewsny@gmail.com>
2022-01-28 13:52:03 -0700

    v27-foo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited and saved 26-master.
+          Edited and saved 27-foo.
         </p>
         <a
           className="App-link"

b3c868c45e84a2f3826d4a7aff05050e85a8c6ae
Bradley Andrews <bandrewsny@gmail.com>
2022-01-29 09:09:49 -0700

    grauer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited and saved 27-foo.
+          Edited and saved 32-grauer. 
         </p>
         <a
           className="App-link"
